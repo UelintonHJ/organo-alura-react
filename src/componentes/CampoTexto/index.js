af99043e25de0b209406757ad28832d90434aff6
UelintonHJ <uelinton.h@gmail.com>
2024-01-12 17:10:55 -0300

    Manipulando input do usuário

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,18 @@
 import './CampoTexto.css'
 
-function CampoTexto(props) {
+const CampoTexto = (props) => {
+    let valor = 'Uelinton Janke'
     return (
         <div className="campoTexto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value={valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
-    )
+    ) 
+}
+
+const aoDigitar = (evento) => {
+    let valor = evento.target.value
+    console.log(valor)
 }
 
 export default CampoTexto
