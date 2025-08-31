dc3bc3d8e47c1d930ead7550d89e71ad81eb6fab
Uelinton Janke <uelinton.h@gmail.com>
2025-08-31 10:07:26 -0300

    Renomeando pastas para lowercase

---
+++ src/componentes/times/index.js
@@ -0,0 +1,16 @@
+import Colaboradores from '../colaboradores'
+import './Times.css'
+
+const Times = (props) => {
+    return (
+        props.colaboradores.length > 0 ? <section className="times" style={{backgroundColor:props.corFundo}}>
+            <h3 style={{borderColor:props.corDestaque}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaboradores key={colaborador.nome} imagem={colaborador.imagem} nome={colaborador.nome} cargo={colaborador.cargo} corDestaque={props.corDestaque}/>)}
+            </div>
+        </section>
+        : ''
+    )
+}
+
+export default Times
